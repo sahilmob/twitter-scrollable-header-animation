3378e7a8579cb3a5297a3f679fe45e675dc9af9b
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-18 19:31:10 +0200

    style header

--- App.js
+++ App.js
@@ -7,7 +7,11 @@ PROFILE_IMAGE_MAX_HEIGHT = 80;
 PROFILE_IMAGE_MIN_HEIGHT = 40;
 export default class App extends Component {
 	render() {
-		return <View style={styles.container} />;
+		return (
+			<View style={styles.container}>
+				<View style={styles.header} />
+			</View>
+		);
 	}
 }
 
@@ -17,5 +21,13 @@ const styles = StyleSheet.create({
 		justifyContent: "center",
 		alignItems: "center",
 		backgroundColor: "#F5FCFF"
+	},
+	header: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		backgroundColor: "lightskyblue",
+		height: HEADER_MAX_HEIGHT
 	}
 });
