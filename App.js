3a88320a380fd228dd3fa8237e5b5d042d7651ba
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-18 19:28:38 +0200

    define constants

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import React, {Component} from 'react';
-import  {StyleSheet, View} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
+HEADER_MAX_HEIGHT = 120;
+HEADER_MIN_HEIGHT = 70;
+PROFILE_IMAGE_MAX_HEIGHT = 80;
+PROFILE_IMAGE_MIN_HEIGHT = 40;
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-
-      </View>
-    );
-  }
+	render() {
+		return <View style={styles.container} />;
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#F5FCFF"
+	}
 });
